84c7cefbc3df5dbcb82b37de2916069f56ac89b7
Zlash65 <zarrar65@gmail.com>
2017-11-02 17:09:42 +0530

    updated module view

---
+++ construction_management/config/construction_management.py
@@ -0,0 +1,49 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Projects"),
+			"icon": "fa fa-cubes",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Project",
+					"description": _("Project master."),
+				},
+				{
+					"type": "doctype",
+					"name": "Task",
+					"route": "Tree/Task",
+					"description": _("Project activity / task."),
+				},
+				{
+					"type": "doctype",
+					"name": "Project Type",
+					"description": _("Define Project type."),
+				}
+			]
+		},
+		{
+			"label": _("Construct"),
+			"icon": "fa fa-building",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Rate Analysis",
+					"description": _("Construction related Rates"),
+				}
+			]
+		},
+		{
+			"label": _("Configure"),
+			"icon": "fa fa-wrench",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "UOM",
+					"description": _("Plan for maintenance visits."),
+				},
+			]
+		}
+	]
